7d1fba994cfec8ad62032f6a76746508a1a52a78
Luís Martins <137302218+luismrtns@users.noreply.github.com>
2026-04-10 18:28:44 -0300

    adição de modo escuro

--- script.js
+++ script.js
@@ -76,3 +76,82 @@ function initAccordion(){
     }
 }
 initAccordion()
+
+const btnDark = document.getElementById('btn-dark')
+const htmlElement = document.documentElement
+
+const temaSalvo = localStorage.getItem('theme')
+if(temaSalvo === 'dark'){
+    htmlElement.classList.add('dark')
+}
+
+const toggleCircle = document.getElementById('toggle-circle')
+const icon = document.getElementById('icon')
+if(localStorage.getItem('theme') === 'dark'){
+    htmlElement.classList.toggle('dark')
+    toggleCircle.classList.add('translate-x-6')
+    icon.textContent = '☀️'
+}
+
+btnDark.addEventListener('click', () => {
+    htmlElement.classList.toggle('dark')
+    const isDark = htmlElement.classList.contains('dark')
+    toggleCircle.classList.toggle('translate-x-6')
+    icon.textContent = isDark ? '🌙' : '☀️'
+    localStorage.setItem('theme', isDark ? 'dark' : 'light')
+})
+
+// const transacao = [
+//     {
+//         descricao: 'taxa do pão',
+//         valor: 'R$ 39',
+//     },
+//     {
+//         descricao: 'taxa do mercado',
+//         valor: 'R$ 129',
+//     },
+//     {
+//         descricao: 'recebimento do cliente',
+//         valor: 'R$ 99',
+//     },
+//     {
+//         descricao: 'taxa do banco',
+//         valor: 'R$ 129',
+//     },
+// ]
+//
+// let taxa = 0
+// transacao.forEach((item) => {
+//     const numeroLimpo = +item.valor.replace('R$ ', '')
+//     if(item.descricao.slice(0,4) === 'taxa'){
+//         taxa += numeroLimpo
+//     }
+// })
+// console.log(taxa)
+//
+// const transportes = 'carro;avião;trem;bicicleta'
+// console.log(transportes.split(';'))
+//
+// let html = `<ul>
+//                         <li><span>Sobre</span></li>
+//                         <li><span>Produtos</span></li>
+//                         <li><span>Contato</span></li>
+//                        </ul>`
+//
+// console.log(html.split('span').join('a'))
+//
+// const frase = 'eu sou corinthians'
+// console.log(frase[frase.length-1])
+//
+// const transacao2 = ['taxa do banco', '    taxa do pão', '   taxa do mercado', 'depósito bancário', 'tarifa especial']
+// let taxaTotal = 0
+// transacao2.forEach((item) => {
+//     if (typeof item === 'string') {
+//         const itemFormatado = String(item.toLowerCase().trim());
+//
+//         if (itemFormatado.includes('taxa')) {
+//             taxaTotal++;
+//         }
+//     }
+// });
+// console.log(taxaTotal)
